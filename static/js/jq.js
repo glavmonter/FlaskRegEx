e5fa00c0c9bca3d6bc8893231074de4ee7e1c25e
Vladimir Meshkov <glavmonter@gmail.com>
2021-05-12 11:44:08 +0300

    Кнопочки

--- static/js/jq.js
+++ static/js/jq.js
@@ -16,3 +16,24 @@ function bufferKey(e) {
 function checkRegex() {
     console.log('checkRegex called')
 }
+
+function toggleFlag(flag, button) {
+    console.log('Clicked ' + flag);
+    flag_input = $('#' + flag);
+
+    if (flag_input.val() == 1) {
+        console.log('Set to 0');
+        flag_input.val(0);
+        $(button).removeClass('checked');
+        $(button).mousedown(function () {
+            $(button).addClass('checked');
+        });
+    } else {
+        console.log('Set to 1');
+        flag_input.val(1);
+        $(button).addClass('checked');
+        $(button).mousedown(function () {
+            $(button).removeClass('checked');
+        });
+    }
+}
